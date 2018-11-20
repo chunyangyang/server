9e449154f65ef427c908a03834b764419037cb75
zheng_chun_yang <zhengchunyang@zuzuche.com>
2018-11-20 23:55:20 +0800

    debug

--- big/src/middleware/strict-uri-encode.js
+++
@@ -1,3 +0,0 @@
-export default ( str ) => {
-    return encodeURIComponent( str ).replace( /[!'()*]/g, x => `%${x.charCodeAt( 0 ).toString( 16 ).toUpperCase()}` );
-};
--- big/src/pages/qian/js/index.view.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Index from '../container/index/app';
-
-ReactDOM.render( 
-    <Index/> ,
-    document.getElementById('root')
-);
